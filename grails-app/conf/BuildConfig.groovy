636655982403ea12389fbd838aef01bb45439f11
markew <mark.woolston@axon.com.au>
2013-01-13 06:32:13 +0000

    move plugin dependencies into BuildConfig.groovy remove hibernate plugin
    git-svn-id: http://ala-visualexplorer.googlecode.com/svn/trunk/tviewer@33 1c5edd6f-5815-6688-b77e-81233d3001a4

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,6 +1,8 @@
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
+grails.project.target.level = 1.6
+grails.project.source.level = 1.6
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
@@ -9,10 +11,14 @@ grails.project.dependency.resolution = {
         // excludes 'ehcache'
     }
     log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+
     repositories {
         grailsPlugins()
         grailsHome()
         grailsCentral()
+
+        mavenLocal()
+        mavenCentral()
 
         // uncomment the below to enable remote dependency resolution
         // from public Maven repositories
@@ -22,10 +28,23 @@ grails.project.dependency.resolution = {
         //mavenRepo "http://repository.codehaus.org"
         //mavenRepo "http://download.java.net/maven/2/"
         //mavenRepo "http://repository.jboss.com/maven2/"
+        mavenRepo "http://maven.ala.org.au/repository"
+
     }
     dependencies {
-        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
+    }
+    plugins {
+        //runtime ":jquery:1.8.0"
+        runtime ":resources:1.1.6"
+        runtime ":rest:0.7"
 
-        // runtime 'mysql:mysql-connector-java:5.1.13'
+        // Uncomment these (or add new ones) to enable additional resources capabilities
+        //runtime ":zipped-resources:1.0"
+        //runtime ":cached-resources:1.0"
+        //runtime ":yui-minify-resources:0.1.4"
+
+        build ":tomcat:$grailsVersion"
+
+        compile ':cache:1.0.1'
     }
 }
